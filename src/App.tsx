a4e2f165249ba1fc043acf7ace87b238cbc36ffc
JessikaAlmgren <jessika.almgren.wennerlund@iths.se>
2021-05-25 19:58:06 +0200

    Foundation av projektet klart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './header/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+       <Header/> 
       </header>
+	  <section className="content">
+	  <h3>Content</h3>
+	  </section>
+	  <section className="footer">
+		  <h1>Footer</h1>
+	  </section>
     </div>
   );
 }
---
+++ src/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+	return (
+	  <div className="App">
+		<header className="App-header">
+		 <h1>Hej</h1>
+		</header>
+	  </div>
+	);
+  }
+
+  export default Header
